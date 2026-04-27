32437bc8e6af2b9084bdeae912440c93aef417a6
Kervin Candido <kervincandido@gmail.com>
2026-04-26 22:42:28 -0300

    subindo git action

--- src/main/java/br/com/fiap/feedback/GreetingResource.java
+++ src/main/java/br/com/fiap/feedback/GreetingResource.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import reactor.core.publisher.Mono;
 
 @Path("/")
 public class GreetingResource {
@@ -16,9 +17,23 @@ public class GreetingResource {
     }
 
     @GET
+    @Path("async/sign-in")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mono<String> signInAsync() {
+        return Mono.just("{\"message\": \"sign-in\"}");
+    }
+
+    @GET
     @Path("sign-up")
     @Produces(MediaType.APPLICATION_JSON)
     public String signUp() {
         return "{\"message\": \"sign-up\"}";
+    }
+
+    @GET
+    @Path("async/sign-up")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Mono<String> signUpAsync() {
+        return Mono.just("{\"message\": \"sign-up\"}");
     }
 }
