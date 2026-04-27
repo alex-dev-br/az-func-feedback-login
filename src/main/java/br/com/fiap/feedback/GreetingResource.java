1c1e0980400502043d2f443c31da0e9820c7b190
Kervin Candido <kervincandido@gmail.com>
2026-04-26 21:09:24 -0300

    add function name

--- src/main/java/br/com/fiap/feedback/GreetingResource.java
+++ src/main/java/br/com/fiap/feedback/GreetingResource.java
@@ -1,5 +1,6 @@
 package br.com.fiap.feedback;
 
+import com.microsoft.azure.functions.annotation.FunctionName;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -9,10 +10,10 @@ import reactor.core.publisher.Mono;
 @Path("/authetication")
 public class GreetingResource {
 
+    @FunctionName("func-feedback-login")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Mono<String> hello() {
         return Mono.just("{\"message\": \"hello\"}");
     }
-
 }
