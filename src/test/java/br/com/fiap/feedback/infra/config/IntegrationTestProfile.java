8b2a07e1ec9a0fdd902841bf9d4fc8ff0620c100
Kervin Candido <kervincandido@gmail.com>
2026-05-01 00:37:57 -0300

    test(config): adiciona chaves publicas para verificacao do jwt nos testes
    Atualiza IntegrationTestProfile com as configuracoes mp.jwt.verify.publickey.location e quarkus.native.resources.includes necessarias para que a validacao do JWT e o build nativo funcionem adequadamente no ambiente de teste.

--- src/test/java/br/com/fiap/feedback/infra/config/IntegrationTestProfile.java
+++ src/test/java/br/com/fiap/feedback/infra/config/IntegrationTestProfile.java
@@ -8,7 +8,10 @@ public class IntegrationTestProfile implements QuarkusTestProfile {
 
     @Override
     public Map<String, String> getConfigOverrides() {
-        return Map.of("smallrye.jwt.sign.key.location", "rsaPrivateKey.pem.example",
-                "smallrye.jwt.encrypt.key.location", "publicKey.pem.example");
+        return Map.of(
+                "smallrye.jwt.sign.key.location", "rsaPrivateKey.pem.example",
+                "smallrye.jwt.encrypt.key.location", "publicKey.pem.example",
+                "mp.jwt.verify.publickey.location", "publicKey.pem.example",
+                "quarkus.native.resources.includes", "publicKey.pem.example");
     }
 }
