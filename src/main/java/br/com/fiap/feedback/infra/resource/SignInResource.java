bf7aa6f3b0483e724d820c9481be2edc801f25e5
Kervin Candido <kervincandido@gmail.com>
2026-05-02 14:32:56 -0300

    feat(observability): integra openTelemetry para metricas e logs
    Substitui extensao do Prometheus por OTLP para padronizacao das metricas via Micrometer. Habilita logs no formato OTLP via application.properties e adiciona SLF4J na classe de signIn para demonstracao da injecao automatica de rastreamento.

--- src/main/java/br/com/fiap/feedback/infra/resource/SignInResource.java
+++ src/main/java/br/com/fiap/feedback/infra/resource/SignInResource.java
@@ -1,15 +1,15 @@
 package br.com.fiap.feedback.infra.resource;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import com.azure.security.keyvault.secrets.SecretClient;
-
 import br.com.fiap.feedback.core.controller.UserController;
 import br.com.fiap.feedback.core.inbound.SignInInput;
 import br.com.fiap.feedback.infra.resource.request.SignInRequest;
 import br.com.fiap.feedback.infra.service.JwtService;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import jakarta.annotation.security.PermitAll;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -23,6 +23,7 @@ public class SignInResource {
 
     private final UserController userController;
     private final JwtService jwtService;
+    private static final Logger LOG = LoggerFactory.getLogger(SignInResource.class);
 
     @Inject
     public SignInResource(UserController userController, JwtService jwtService) {
@@ -33,10 +34,17 @@ public class SignInResource {
     @POST
     @PermitAll
     @Path("sign-in")
-    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Counted(value = "logins.attempted", description = "Quantidade de tentativas de login (com e sem sucesso)")
+    @Timed(value = "logins.duration", description = "Tempo de processamento do login", histogram = true)
     public Response signIn(@Valid SignInRequest request) {
+        LOG.info("Iniciando tentativa de login para o usuário: {}", request.username());
+        
         var userOutput = userController.signIn(new SignInInput(request.username(), request.password()));
+        
+        LOG.info("Login efetuado com sucesso para o usuário: {}", request.username());
+        
         var jwtBearerToken = jwtService.generateJwtBearerToken(userOutput);
         return Response.ok().entity(jwtBearerToken).build();
     }
